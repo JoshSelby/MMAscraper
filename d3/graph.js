6a66666efea936ca5907859d5724a783a080a638
JoshSelby <43355360+JoshSelby@users.noreply.github.com>
2018-09-27 12:29:31 -0400

    Nice static graph

--- d3/graph.js
+++ d3/graph.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 50, right: 100, bottom: 50, left: 50},
+var margin = {top: 50, right: 150, bottom: 50, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -13,7 +13,8 @@ var y = d3.scaleLinear().range([height, 0]);
 // define the line
 var valueline = d3.line()
     .x(function(d) { return x(d.Date); })
-    .y(function(d) { return y(d.rating); });
+    .y(function(d) { return y(d.rating); })
+    .curve(d3.curveStepAfter);
 
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
@@ -39,8 +40,12 @@ d3.csv("fightsEloLong2.csv", function(error, data) {
   });
 
   // Scale the range of the data
-  x.domain(d3.extent(data, function(d) { return d.Date; }));
-  y.domain(d3.extent(data, function(d) { return d.rating; }));
+  x.domain([d3.min(data, function(d) { return d.Date }),
+            d3.max(data, function(d) { return d.Date })]);
+  y.domain([d3.min(data, function(d) { return d.rating }),
+            d3.max(data, function(d) { return d.rating })]);
+
+  console.log([1000,d3.max(data, function(d) { return d.rating })]);
 
   // Nest the entries by
   var dataNest = d3.nest()
@@ -71,39 +76,16 @@ d3.csv("fightsEloLong2.csv", function(error, data) {
 
       svg.append("text")
           .attr("transform", "translate(" +
-              (x(data[data.length-1].Date)+10) + "," +
-              y(data[data.length-1].rating) + ")")
+              (x(d.values[d.values.length-1].Date)+10) + "," +
+              y(d.values[d.values.length-1].rating) + ")")
           .attr("dy", ".35em")
           .attr("text-anchor", "start")
-          .style("fill", "red")
-          .text(dataNest[0].key);
-
-      console.log(dataNest[0].key);
-
-
-      // Add the Legend
-      // svg.append("text")
-      //     .attr("x", (legendSpace/2)+i*legendSpace)  // space legend
-      //     .attr("y", height + (margin.bottom/2)+ 5)
-      //     .attr("class", "legend")    // style the legend
-      //     .style("fill", function() { // Add the colours dynamically
-      //         return d.color = color(d.key); })
-      //     .on("click", function(){
-      //         // Determine if current line is visible
-      //         var active   = d.active ? false : true,
-      //         newOpacity = active ? 0 : 1;
-      //         // Hide or show the elements based on the ID
-      //         d3.select("#tag"+d.key.replace(/\s+/g, ''))
-      //             .transition().duration(100)
-      //             .style("opacity", newOpacity);
-      //         // Update whether or not the elements are active
-      //         d.active = active;
-      //         })
-      //     .text(d.key);
+          .style("fill", function() { // Add the colours dynamically
+              return d.color = color(d.key); })
+          .text(d.values[d.values.length-1].Link + ", " +
+                d.values[d.values.length-1].rating);
 
   });
-
-
 
   // Add the X Axis
   svg.append("g")
