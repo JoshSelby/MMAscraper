f69abb8f2a1ef1ed2858d3735f2945e6dacd25ec
JoshSelby <43355360+JoshSelby@users.noreply.github.com>
2018-10-01 13:10:50 -0400

    moving gridlines

--- d3/graph2.js
+++ d3/graph2.js
@@ -1,202 +1,100 @@
-// set the dimensions and margins of the graph
-var margin = {top: 50, right: 150, bottom: 50, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var data = [];
+var time_frame = 10000;
+var height = 400;
+var width = 700;
+var margin = 30;
+var duration = 1000;
+var time = Date.now();
 
-// parse the date
-var parseDate = d3.timeParse("%Y-%m-%d");
+var xScale = d3.scaleTime()
+    .domain([time + duration, time - (duration * 2) - time_frame])
+    .range([width - (margin *2), 0]);
+var yScale = d3.scaleLinear()
+    .domain([0,10])
+    .range([height - (margin*2), 0]);
 
-// set the ranges
-var x = d3.scaleTime()
-          .range([0, width]);
-var y = d3.scaleLinear()
-          .range([height, 0]);
+var xAxis = d3.axisBottom(xScale)
+    .ticks(4);
 
-var yAxis = d3.axisLeft(y)
-              .scale(y);
-var xAxis = d3.axisBottom(x)
-              .scale(x);
+var yAxis = d3.axisRight(yScale)
+    .ticks(4);
 
-// define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d.Date); })
-    .y(function(d) { return y(d.rating); })
-    .curve(d3.curveStepAfter);
+var svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g');
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+svg.append('g')
+    .attr('transform', 'translate(' + (width - margin * 2) + ',' + margin + ')')
+    .classed('y axis', true)
+    .call(yAxis);
 
-var circleTransition = d3.select("circle")
-                         .transition();
+svg.append('g')
+    .attr('transform', 'translate(0,' + (height - margin) + ')')
+    .classed('x axis', true);
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+svg.append('defs')
+    .append('clipPath')
+    .attr('id', 'clip')
+    .append('rect')
+    .attr('width', (width - (margin * 4)))
+    .attr('height', (height - (margin * 2)))
+    .attr('transform', 'translate(-1.5, 20)');
 
-var t = 2000;
+svg.append('g')
+    .attr('clip-path', 'url(#clip)')
+    .classed('line_', true)
+    .append('path')
+    .datum(data)
+    .classed('line', true)
+    .style('fill', 'none')
+    .style('stroke', 'steelblue')
+    .style('stroke-width', '1.5px');
 
-// gridlines in x axis function
-function make_x_gridlines() {
-    return d3.axisBottom(x)
-        .ticks(5)
+var interval = d3.interval(update, 1000);
+
+function update(){
+    time = Date.now();
+    data.push({'x': time, 'y': d3.randomUniform(0,9)()});
+    draw();
 }
 
-// gridlines in y axis function
-function make_y_gridlines() {
-    return d3.axisLeft(y)
-        .ticks(5)
-}
+function draw(){
+    var line = d3.line()
+        .x(function(d) { return xScale(d.x); })
+        .y(function(d) { return yScale(d.y); });
+
+    var lineselection = svg.selectAll('.line_')
+        .select('path');
 
 
-d3.csv("fightsEloLong2.csv", function(error, data) {
-  if (error) throw error;
-
-  // format the data
-  data.forEach(function(d) {
-      d.Link = d.Link.replace(/\d|-/g, " ").trim(),
-      d.Date = parseDate(d.Date);
-      d.rating = +d.rating;
-  });
-
-
-  var i = 1;
-  function dataNestLoop(limit) {
-    var limit = data.length+1;
-    var ref = setInterval(() => {
-      dataNest = d3.nest()
-        .key(function(d) {return d.Link})
-        .entries(data.slice(0,i));
-      update();
-      createGraph();
-      i++;
-      if (i == limit) clearInterval(ref);
-    }, t);
-  };
-
-  function update (){
-    minDate = d3.min(data.slice(0,i), function(d) { return d.Date });
-    maxDate = d3.max(data.slice(0,i), function(d) { return d.Date });
-    minrating = d3.min(data.slice(0,i), function(d) { return d.rating });
-    maxrating = d3.max(data.slice(0,i), function(d) { return d.rating });
-
-    yearMax = maxDate.getFullYear()
-    monthMax = maxDate.getMonth()
-    dayMax = maxDate.getDate()
-
-    // Scale the range of the data
-    x.domain([new Date(yearMax - 1, monthMax + 1, dayMax),
-                 new Date(yearMax, monthMax + 1, dayMax)]);
-    y.domain([minrating, maxrating]);
-
-  }
-
-  function createGraph() {
-
-    // Plot points and lines
-    dataNest.forEach(function(d,i) {
-      // data-join
-      var dot = svg.selectAll("dot")
-          .data(d.values);
-      console.log(dataNest);
-
-      // update
-      dot.transition(t)
-          .ease(d3.easeLinear)
-          .attr("r", 5)
-          .attr("cx", function(d) { return x(d.Date); })
-          .attr("cy", function(d) { return y(d.rating); })
-          .style("fill", function() { // Add the colours dynamically
-              return d.color = color(d.key); });
-
-      // enter
-      dot.enter().append("circle")
-          .attr("r", 3)
-          .attr("cx", function(d) { return x(d.Date); })
-          .attr("cy", function(d) { return y(d.rating); })
-          .style("fill", function() { // Add the colours dynamically
-              return d.color = color(d.key); })
-          .style("stroke", "black")
-          .style("stroke-width", "1")
-        .transition()
+    lineselection.interrupt()
+    		.transition()
+        .duration(duration)
         .ease(d3.easeLinear)
-          .duration(t)
-          .remove();
+        //.attr('transform', 'translate(' + -(xScale.range()[0]/((duration / 100)-2)) + ',' + margin + ')');
+        .attr('transform', 'translate(' + -(xScale(data[data.length-1].x) -xScale.range()[0]) + ',' + margin + ')');
 
 
-        svg.append("path")
-            .attr("class", "line")
-            .style("stroke", function() { // Add the colours dynamically
-                return d.color = color(d.key); })
-            .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
-            .attr("d", valueline(d.values))
-            .transition()
-              .ease(d3.easeLinear)
-              .duration(t)
-              .remove();
+    if (data[0].x < time - time_frame - duration ){
+            console.log('shift');
+                data.shift();
+    }
+
+    lineselection.attr('d',line)
+        .attr('transform', 'translate(0,' + margin + ')');
 
 
-        svg.append("text")
-            .attr("transform", "translate(" +
-                (x(d.values[d.values.length-1].Date)+10) + "," +
-                y(d.values[d.values.length-1].rating) + ")")
-            .attr("dy", ".35em")
-            .attr("text-anchor", "start")
-            .style("fill", function() { // Add the colours dynamically
-                return d.color = color(d.key); })
-            .text(d.values[d.values.length-1].Link + ", " +
-                  d.values[d.values.length-1].rating)
-            .transition()
-              .duration(t)
-              .remove();
+    xScale.domain([time, time + (duration * 2) - time_frame])
+        .range([width - (margin *2),0]);
 
-        svg.select(".x")
-           .transition(t)
-             .ease(d3.easeLinear)
-           .call(xAxis);
-
-        svg.select(".y")
-           .transition(t)
-             .ease(d3.easeLinear)
-           .call(yAxis);
-
-
-        });
-
-  };
-
-
-  dataNestLoop();
-
-  // add the X gridlines
-  svg.append("g")
-      .attr("class", "grid")
-      .attr("transform", "translate(0," + height + ")")
-      .call(make_x_gridlines()
-          .tickSize(-height)
-          .tickFormat("")
-      )
-
-  // add the Y gridlines
-  svg.append("g")
-      .attr("class", "grid")
-      .call(make_y_gridlines()
-          .tickSize(-width)
-          .tickFormat("")
-      )
-
-  svg.append("g")
-      .attr("class", "axis x")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  svg.append("g")
-      .attr("class", "axis y")
-      .call(yAxis);
-
-
-});
+    d3.select('body')
+        .select('svg')
+        .select('g')
+        .selectAll('.x.axis')
+        .transition()
+        .duration(duration)
+        .ease(d3.easeLinear)
+        .call(xAxis);
+}
--- d3/graph3.js
+++ d3/graph3.js
@@ -1,6 +1,6 @@
 var t = 0,
-ticksx = 36,
-duration = 500;
+ticksx = 6,
+duration = 2000;
 // parse the date
 var parseDate = d3.timeParse("%Y-%m-%d");
 
@@ -11,56 +11,59 @@ function refilter() {
       width = 1000 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
- var x = d3.scaleTime()
-     .domain([new Date(2000, 0, 1), new Date(2000, ticksx, 1)])
+  var x = d3.scaleTime()
+     .domain([new Date(2005, 0, 1), new Date(2005, ticksx, 1)])
      .range([0, width]);
 
- var y = d3.scaleLinear()
+  var y = d3.scaleLinear()
     .domain([1000,2300])
     .range([height, 0]);
 
- var xAxis = d3.axisBottom()
+  var xAxis = d3.axisBottom()
     .scale(x)
-    .ticks(5)
-    .tickFormat(d3.timeFormat("%b-%d-%Y"));
+    .tickFormat(d3.timeFormat("%b %Y"))
+    .tickSize(6);
 
- var yAxis = d3.axisRight()
+  var yAxis = d3.axisLeft()
      .scale(y)
      .ticks(5);
 
- var svg = d3.select("body").append("svg")
-     .attr("width", width + margin.left + margin.right)
+  var svg = d3.select("body").append("svg")
+     .attr("width", width + margin.left)
      .attr("height", height + margin.top + margin.bottom)
    .append("g")
      .attr("transform",
            "translate(" + margin.left + "," + margin.top + ")");
 
+
+  var chartGroup = d3.select("body").append("svg")
+     .attr("width", width - 200)
+     .attr("height", height + margin.top + margin.bottom)
+     .attr("transform", "translate(-"+ (width) +",0)")
+     .append("g");
+
   // extra svg to clip the graph and x axis as they transition in and out
   var graph = svg.append("svg")
-    .attr("width", width + margin.left)
+    .attr("width", width)
     .attr("height", height + margin.top);
 
-  var xaxis = graph.append("g")
+  var xaxis = svg.append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(-10," + (height+10) + ")")
+    .attr("transform", "translate(0," + (height) + ")")
     .call(xAxis);
 
-  var yaxis = graph.append("g")
+  var yaxis = svg.append("g")
       .attr("class", "y axis")
-      .attr("transform", "translate("+(width-10)+",10)")
+      .attr("transform", "translate("+(width)+",0)")
       .call(yAxis);
 
-  // gridlines in x axis function
-  function make_x_gridlines() {
-      return d3.axisBottom(x)
-          .ticks(5)
-  }
-
-  // gridlines in y axis function
-  function make_y_gridlines() {
-      return d3.axisLeft(y)
-          .ticks(5)
-  }
+  var xTickLine = graph.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0,0)")
+        .call(d3.axisBottom()
+          .scale(x)
+          .tickSize(height)
+          .tickFormat(""));
 
   // define the line
   var valueline = d3.line()
@@ -69,13 +72,6 @@ function refilter() {
       .curve(d3.curveStepAfter);
 
   var color = d3.scaleOrdinal(d3.schemeCategory10);
-
-  var chartGroup = svg.append("g")
-      .attr("width", width)
-      .attr("height", height + margin.top);
-
-
-
 
   //////////////////////////////////////////////////////////////////////////////
 
@@ -95,14 +91,13 @@ function refilter() {
 
     // Plot points and lines
     dataNest.forEach(function(d,i) {
-
         chartGroup.append("path")
             .attr("class", "line")
             .style("stroke", function() { // Add the colours dynamically
                 return d.color = color(d.key); })
             .attr("id", 'tag'+d.key.replace(/\s+/g, '')) // assign ID
             .attr("d", valueline(d.values))
-            .attr("width",500);
+            .attr("width",50);
 
         chartGroup.selectAll("dot")
             .data(d.values)
@@ -115,7 +110,7 @@ function refilter() {
             .style("stroke", "black")
             .style("stroke-width", "1");
 
-        svg.append("text")
+        chartGroup.append("text")
             .attr("transform", "translate(" +
                 (x(d.values[d.values.length-1].Date)+10) + "," +
                 y(d.values[d.values.length-1].rating) + ")")
@@ -125,8 +120,9 @@ function refilter() {
                 return d.color = color(d.key); })
             .text(d.values[d.values.length-1].Link + ", " +
                   d.values[d.values.length-1].rating);
-
     });
+
+
 
     function tick() {
       t++;
@@ -141,21 +137,26 @@ function refilter() {
           return d.Date >= curDx[0] & d.Date <= curDx[1] }),
           function(d) {return d.rating});
 
-
       var curDy = y.domain();
       var newDy = [curminRate, curmaxRate];
 
       x.domain(newDx);
 
-      console.log(y.domain());
-      shiftX = newDx[0] - curDx[1].valueOf()
+      shiftX = newDx[0] - curDx[1].valueOf();
 
       chartGroup.transition()
-                .attr("transform", "translate("+ -width/ticksx  * t +",0)")
-                .duration(duration)
-                .ease(d3.easeLinear);
+          .attr("transform", "translate("+ -width/ticksx  * t +")")
+          .duration(duration)
+          .ease(d3.easeLinear);
 
-      // slide the x-axis left
+      xTickLine.transition()
+          .duration(duration)
+          .ease(d3.easeLinear)
+          .call(d3.axisBottom()
+            .scale(x)
+            .tickSize(height)
+            .tickFormat(""));
+
       xaxis.transition()
           .duration(duration)
           .ease(d3.easeLinear)
@@ -166,18 +167,9 @@ function refilter() {
           .ease(d3.easeLinear)
           .call(yAxis)
           .on("end", tick);
-
-
     }
-
-
     tick();
-
-
-
   });
   //////////////////////////////////////////////////////////////////////////////
-
 }
-
 refilter();
