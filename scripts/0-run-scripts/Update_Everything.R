3b6841fb59a7cd06d0c2ef537bd7859580ea98a5
JoshSelby <anorth3164@gmail.com>
2019-02-22 19:10:35 -0500

    .

--- scripts/0-run-scripts/Update_Everything.R
+++ scripts/0-run-scripts/Update_Everything.R
@@ -7,12 +7,12 @@ source('./scripts/3-records/recordAdder.R', echo=TRUE)
 source('./scripts/4-ratings/add_ratings.R', echo=TRUE)
 source('./scripts/5-metrics/metrics.R', echo=TRUE)
 source('./scripts/5-metrics/events.R', echo=TRUE)
-source('./scripts/7-scrape-odds/futureOdds.R', echo=TRUE)
 source('./scripts/7-scrape-odds/updateOdds.R', echo=TRUE)
 source('./scripts/8-append-odds/SherdogToBFO.R', echo=TRUE)
 source('./scripts/9-various-tables/Current Stats.R', echo = TRUE)
 source('./scripts/9-various-tables/Add Gender.R', echo = TRUE)
 
+source('./scripts/7-scrape-odds/futureOdds.R', echo=TRUE)
 source('./scripts/10-future-fights/futureFights.R', echo = TRUE)
 
 
